f212535cdc9a5ea0b70f2547629118545b5aa8ce
gheeeeeeee <gheoputra55@gmail.com>
2024-03-08 15:11:56 +0700

    deklarasi variabel global

--- 058_bubblesort/058_bubblesort.cpp
+++ 058_bubblesort/058_bubblesort.cpp
@@ -1,7 +1,12 @@
 // 058_bubblesort.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <iostream>#
+#include <string>
+using namespace std;
+
+int arr[20]; // Deklarasi variabel global array a dengan ukuran 20
+int n; // Deklarasi variabel global n untuk menyimpan banyaknya elemen array
 
 int main()
 {
